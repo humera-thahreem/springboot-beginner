c16b452fb07ba704fe078d2a93f531c4e763c424
Humera Thahreem <your-humerathahreem2786@gmail.com>
2026-07-28 22:47:17 +0530

    created toggle card with functionality

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,1 +1,28 @@
-//alert("Hello from javascript")
+// alert("Hello from javaScript");
+
+var users = [
+    {
+        "image" : "/images/john.png",
+        "name" : "John Doe",
+        "gender" : "Male"
+    },
+    {
+        "image" : "/images/jane.png",
+        "name" : "Jane Doe",
+        "gender" : "Female"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1) % users.length;
+    var userImage = document.getElementById("user-image");
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+
+    userImage.src = users[id].image;
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+
+}
