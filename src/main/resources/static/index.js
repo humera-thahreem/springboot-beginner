9cf136423e0c78574f01db9d4d0bea6ae338aa42
Humera Thahreem <your-humerathahreem2786@gmail.com>
2026-07-29 17:00:37 +0530

    created random user card

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -24,5 +24,27 @@ function toggleUser(){
     userImage.src = users[id].image;
     userName.innerHTML = users[id].name;
     userGender.innerHTML = users[id].gender;
+}
+
+function randomUser(){
+    console.log("function called");
+    fetch("https://randomuser.me/api")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var userImage = document.getElementById("user-image");
+            var userName = document.getElementById("user-name");
+            var userGender = document.getElementById("user-gender");
+
+            userImage.src = data.results[0].picture.large;
+            userName.innerHTML = data.results[0].name.first + " " + data.results[0].name.second;
+            userGender.innerHTML = data.results[0].gender;
+
+        })
+        .error(function(err){
+            console.log(err);
+        })
+
 
 }
